3f355d84a5b792268d37d78e3de4f3eb76595a5b
17YQ <1347880415@qq.com>
2020-04-30 14:50:19 +0800

    This is a test

--- src/main/java/com/roy/App.java
+++ src/main/java/com/roy/App.java
@@ -1,6 +1,7 @@
 package com.roy;
 
 import com.roy.myService.Test;
+import com.roy.myService.Test1;
 
 /**
  * Hello world!
@@ -11,7 +12,9 @@ public class App
     public static void main( String[] args )
     {
         Test t=new Test();
+        Test1 t1=new Test1();
         System.out.println( "GitHub上传项目测试" );
         System.out.println(t.getMe());
+        System.out.println(t1.getMe1());
     }
 }
---
+++ src/main/java/com/roy/myService/Test1.java
@@ -0,0 +1,7 @@
+package com.roy.myService;
+
+public class Test1 {
+    public String getMe1(){
+        return "This is a test";
+    }
+}
