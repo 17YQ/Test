fec1fb4e04625ae7aa12b71937919bf44994836b
17YQ <1347880415@qq.com>
2020-04-30 14:25:08 +0800

    17Test GitHub

--- src/main/java/com/roy/App.java
+++ src/main/java/com/roy/App.java
@@ -1,5 +1,7 @@
 package com.roy;
 
+import com.roy.myService.Test;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,8 @@ public class App
 {
     public static void main( String[] args )
     {
+        Test t=new Test();
         System.out.println( "GitHub上传项目测试" );
+        System.out.println(t.getMe());
     }
 }
---
+++ src/main/java/com/roy/myService/Test.java
@@ -0,0 +1,7 @@
+package com.roy.myService;
+
+public class Test {
+    public String getMe(){
+        return "This is a test project ";
+    }
+}
